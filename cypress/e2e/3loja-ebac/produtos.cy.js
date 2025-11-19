f5ca69a3b498a0d7343dcf1e370715de7522b564
Aline Bierhals <alineevieiraa@hotmail.com>
2025-11-19 09:37:01 -0300

    Listas de produtos

---
+++ cypress/e2e/3loja-ebac/produtos.cy.js
@@ -0,0 +1,19 @@
+/// <reference:cypress/types" />
+
+describe('Funcionalidade: Produtos', () => {
+
+beforeEach(() => {
+    cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+});
+
+    it('Deve selecionar um produto da lista', () => {
+      cy.get('.product-block > .block-inner > .image > .product-image > .image-hover')
+      //.first()
+      //.last()
+      .eq(3)
+      .click()
+      cy.get('#tab-title-description > a').should('contain', 'Descrição')
+
+
+    });
+})
